6c1e4decdc6318b2e1bfed96d899c9724429ac9c
Kyle Rebstock <kjr247@gmail.com>
2024-05-17 13:53:03 -0400

    changes to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -33,22 +33,33 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-
-        <Techstack />
-
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-        <Toolstack />
 
         <Github />
+          <Card className="quote-card-view">
+            <Card.Body>
+              <p>A bit about me.</p>
+              <ul>
+                <li className="about-activity">
+                  <ImPointRight /> I find a great deal of pleasure in being an inspiration, source of energy, and motivation to those around me. I enjoy anything competitive, as well as some relaxing walks/runs while listening to podcasts or books. I am avid about sports and run a soccer league. I love freediving, golf, tennis, rock climbing, boats, cars. I love to teach. For the last 5 years, I have spent about 3 hours a week teaching software and soft skills 1 on 1 with several mentees. As a very curious person, lately, I have been enthralled with Data Science. 
+                </li>
+              </ul>
+            </Card.Body>
+          </Card>
+
+          <Card className="quote-card-view">
+            <Card.Body>
+              <p>Obsessed with Tech</p>
+              <ul>
+                <li className="about-activity">
+                  <ImPointRight /> As soon as I’m awake roughly 5:00 am EST, with coffee and bagel in hand, I am already reading Hacker News, and getting updates on the latest and greatest in Tech. One of my favorite books is The 5 AM Club (5AC!). I have a daily routine of compulsively scouring through the kaleidoscope of docs, Github issues, and online community backlogs (Discord, HackerNews, RSS feeds, Quora, StackOverflow, devdocs.io). From home, I often experiment with cutting-edge and bleeding-edge technologies. I believe that understanding the difference between the two is a critical distinction to make.
+                </li>
+              </ul>
+            </Card.Body>
+          </Card>
 
         <Card className="quote-card-view">
           <Card.Body>
-            <p>Apart from coding, some other activities I enjoy:</p>
+            <p>Apart from building great teams and building great software, some other activities I enjoy:</p>
             <ul>
               <li className="about-activity">
                 <ImPointRight /> Reading and Writing Tech Blogs and building things with my hands!
@@ -59,9 +70,23 @@ function About() {
               <li className="about-activity">
                 <ImPointRight /> Traveling (7 countries and 20 states so far)
               </li>
+              <li className="about-activity">
+                <ImPointRight /> Playing instruments including piano, guitar, and ukulele.
+              </li>              
             </ul>
-            </Card.Body>
-          </Card>
+          </Card.Body>
+        </Card>
+
+        <h1 className="project-heading">
+          Professional <strong className="purple">Skillset </strong>
+        </h1>
+
+        <Techstack />
+
+        <h1 className="project-heading">
+          <strong className="purple">Tools</strong> I use
+        </h1>
+        <Toolstack />
       </Container>
     </Container>
   );
